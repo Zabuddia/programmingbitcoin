db03cfa601deb3ce301dbd7143cb0c7dd79b780f
Zabuddia <fife.alan@gmail.com>
2024-02-06 18:55:43 -0700

    Did chapter 6

--- code-ch06/helper.py
+++ code-ch06/helper.py
@@ -2,6 +2,8 @@ from unittest import TestCase, TestSuite, TextTestRunner
 
 import hashlib
 
+from Crypto.Hash import RIPEMD160
+
 
 SIGHASH_ALL = 1
 SIGHASH_NONE = 2
@@ -16,8 +18,10 @@ def run(test):
 
 
 def hash160(s):
-    '''sha256 followed by ripemd160'''
-    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
+    sha = hashlib.sha256(s).digest()
+    ripe = RIPEMD160.new()
+    ripe.update(sha)
+    return ripe.digest()
 
 
 def hash256(s):
